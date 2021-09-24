0e97463207941ed7215e7394756e1a60115373a6
zhangyukun <zhangyukun8@huawei.com>
2021-09-24 14:48:14 +0800

    Add thread check of ecmascript Signed-off-by: zhangyukun <zhangyukun8@huawei.com>
    Change-Id: I6b954f23e79c0fb98091dc25bc772047489d35bc

--- ecmascript/js_thread.h
+++ ecmascript/js_thread.h
@@ -178,6 +178,16 @@ public:
         return internalCallParams_;
     }
 
+    ThreadId GetThreadId() const
+    {
+        return GetId();
+    }
+
+    static ThreadId GetCurrentThreadId()
+    {
+        return os::thread::GetCurrentThreadId();
+    }
+
     void IterateWeakEcmaGlobalStorage(const WeakRootVisitor &visitor);
 
 private:
