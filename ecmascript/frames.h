bb2aebfbe3a58bbc152d5e3f1c2146bf44764c50
surpassgoodchao <surpassgoodchao@qq.com>
2021-10-22 11:00:53 +0800

    review
    Signed-off-by: surpassgoodchao <surpassgoodchao@qq.com>

--- ecmascript/frames.h
+++ ecmascript/frames.h
@@ -220,7 +220,7 @@
 #define POINTER_CAST(fp, type) static_cast<type>(static_cast<void *>(fp)
 #define GET_PREV_FP(fp)  reinterpret_cast<uintptr_t *>(*(fp))
 #else
-#define GET_CURRETN_FP(fp) 
+#define GET_CURRETN_FP(fp)
 #define POINTER_CAST(fp, type) static_cast<type>(static_cast<void *>(fp))
 #define GET_PREV_FP(fp)  reinterpret_cast<uintptr_t *>(*(POINTER_CAST(fp, uintptr_t *)))
 #endif
