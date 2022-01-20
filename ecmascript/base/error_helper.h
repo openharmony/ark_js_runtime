6ed37cba7c45f2e2fd9fe87e7cac7eafa955c4a2
shikai <shikai25@huawei.com>
2022-01-20 22:59:01 +0800

    Signed-off-by: shikai <shikai25@huawei.com>
    modify

--- ecmascript/base/error_helper.h
+++ ecmascript/base/error_helper.h
@@ -32,8 +32,6 @@ public:
     static JSTaggedValue ErrorCommonConstructor(EcmaRuntimeCallInfo *argv, const ErrorType &errorType);
 
     static CString BuildNativeEcmaStackTrace(JSThread *thread);
-
-    static std::string BuildNativeAndJsBackStackTrace(JSThread *thread);
 private:
     static CString DecodeFunctionName(const CString &name);
     static JSHandle<EcmaString> BuildEcmaStackTrace(JSThread *thread);
