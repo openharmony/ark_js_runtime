1ec31e6f053335a962ed8fb4b376e5a32d1f9dc4
xliu <liuxin259@huawei.com>
2022-02-10 15:24:19 +0800

    Add tests for TreeMap and TreeSet
    Description
      To ensure the high performance of container classes, TreeMap and TreeSet is provided in ark.
      1. Add test cases for ContainerTreeMap.
      2. Add test cases for ContainerTreeSet.
      3. Add test cases for JSAPITreeMap.
      4. Add test cases for JSAPITreeSet.
    Related issue
      #I4T5I6:add test cases for TreeMap and TreeSet
    
    Signed-off-by: xliu <liuxin259@huawei.com>
    Change-Id: Ib01adfd9038498a88785bdd878e524e02ab38dc8

---
+++ ecmascript/tests/js_api_tree_set_test.cpp
@@ -0,0 +1,287 @@
+/*
+ * Copyright (c) 2021 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "ecmascript/containers/containers_private.h"
+#include "ecmascript/ecma_string.h"
+#include "ecmascript/ecma_vm.h"
+#include "ecmascript/global_env.h"
+#include "ecmascript/js_api_tree_set.h"
+#include "ecmascript/js_api_tree_set_iterator.h"
+#include "ecmascript/js_function.h"
+#include "ecmascript/js_handle.h"
+#include "ecmascript/js_iterator.h"
+#include "ecmascript/js_object-inl.h"
+#include "ecmascript/js_tagged_value.h"
+#include "ecmascript/object_factory.h"
+#include "ecmascript/tagged_tree-inl.h"
+#include "ecmascript/tests/test_helper.h"
+
+using namespace panda;
+
+using namespace panda::ecmascript;
+
+namespace panda::test {
+class JSAPITreeSetTest : public testing::Test {
+public:
+    static void SetUpTestCase()
+    {
+        GTEST_LOG_(INFO) << "SetUpTestCase";
+    }
+
+    static void TearDownTestCase()
+    {
+        GTEST_LOG_(INFO) << "TearDownCase";
+    }
+
+    void SetUp() override
+    {
+        TestHelper::CreateEcmaVMWithScope(instance, thread, scope);
+    }
+
+    void TearDown() override
+    {
+        TestHelper::DestroyEcmaVMWithScope(instance, scope);
+    }
+
+    PandaVM *instance {nullptr};
+    ecmascript::EcmaHandleScope *scope {nullptr};
+    JSThread *thread {nullptr};
+
+protected:
+    JSAPITreeSet *CreateTreeSet()
+    {
+        ObjectFactory *factory = thread->GetEcmaVM()->GetFactory();
+        JSHandle<GlobalEnv> env = thread->GetEcmaVM()->GetGlobalEnv();
+
+        JSHandle<JSTaggedValue> globalObject = env->GetJSGlobalObject();
+        JSHandle<JSTaggedValue> key(factory->NewFromCanBeCompressString("ArkPrivate"));
+        JSHandle<JSTaggedValue> value =
+            JSObject::GetProperty(thread, JSHandle<JSTaggedValue>(globalObject), key).GetValue();
+
+        auto objCallInfo = TestHelper::CreateEcmaRuntimeCallInfo(thread, JSTaggedValue::Undefined(), 6);
+        objCallInfo->SetFunction(JSTaggedValue::Undefined());
+        objCallInfo->SetThis(value.GetTaggedValue());
+        objCallInfo->SetCallArg(0, JSTaggedValue(static_cast<int>(containers::ContainerTag::TreeSet)));
+
+        [[maybe_unused]] auto prev = TestHelper::SetupFrame(thread, objCallInfo.get());
+        JSTaggedValue result = containers::ContainersPrivate::Load(objCallInfo.get());
+        TestHelper::TearDownFrame(thread, prev);
+
+        JSHandle<JSTaggedValue> constructor(thread, result);
+        JSHandle<JSAPITreeSet> set(factory->NewJSObjectByConstructor(JSHandle<JSFunction>(constructor), constructor));
+        JSTaggedValue internal = TaggedTreeSet::Create(thread);
+        set->SetTreeSet(thread, internal);
+        return *set;
+    }
+};
+
+HWTEST_F_L0(JSAPITreeSetTest, TreeSetCreate)
+{
+    JSAPITreeSet *set = CreateTreeSet();
+    EXPECT_TRUE(set != nullptr);
+}
+
+HWTEST_F_L0(JSAPITreeSetTest, TreeSetAddAndHas)
+{
+    constexpr uint32_t NODE_NUMBERS = 8;
+    ObjectFactory *factory = thread->GetEcmaVM()->GetFactory();
+    JSMutableHandle<JSTaggedValue> key(thread, JSTaggedValue::Undefined());
+
+    // test JSAPITreeSet
+    JSHandle<JSAPITreeSet> tset(thread, CreateTreeSet());
+    std::string myKey("mykey");
+    for (uint32_t i = 0; i < NODE_NUMBERS; i++) {
+        std::string ikey = myKey + std::to_string(i);
+        key.Update(factory->NewFromStdString(ikey).GetTaggedValue());
+        JSAPITreeSet::Add(thread, tset, key);
+    }
+    EXPECT_EQ(tset->GetSize(), NODE_NUMBERS);
+
+    for (uint32_t i = 0; i < NODE_NUMBERS; i++) {
+        std::string ikey = myKey + std::to_string(i);
+        key.Update(factory->NewFromStdString(ikey).GetTaggedValue());
+
+        // test has
+        bool has = JSAPITreeSet::Has(thread, tset, key);
+        EXPECT_EQ(has, true);
+    }
+}
+
+HWTEST_F_L0(JSAPITreeSetTest, TreeSetDeleteAndHas)
+{
+    constexpr uint32_t NODE_NUMBERS = 64;
+    constexpr uint32_t REMOVE_SIZE = 48;
+    ObjectFactory *factory = thread->GetEcmaVM()->GetFactory();
+    JSMutableHandle<JSTaggedValue> key(thread, JSTaggedValue::Undefined());
+
+    // test JSAPITreeSet
+    JSHandle<JSAPITreeSet> tset(thread, CreateTreeSet());
+    std::string myKey("mykey");
+    for (uint32_t i = 0; i < NODE_NUMBERS; i++) {
+        std::string ikey = myKey + std::to_string(i);
+        key.Update(factory->NewFromStdString(ikey).GetTaggedValue());
+        JSAPITreeSet::Add(thread, tset, key);
+    }
+    EXPECT_EQ(tset->GetSize(), NODE_NUMBERS);
+
+    for (uint32_t i = 0; i < REMOVE_SIZE; i++) {
+        std::string ikey = myKey + std::to_string(i);
+        key.Update(factory->NewFromStdString(ikey).GetTaggedValue());
+        bool success = JSAPITreeSet::Delete(thread, tset, key);
+        EXPECT_EQ(success, true);
+    }
+    EXPECT_EQ(tset->GetSize(), NODE_NUMBERS - REMOVE_SIZE);
+
+    for (uint32_t i = 0; i < REMOVE_SIZE; i++) {
+        std::string ikey = myKey + std::to_string(i);
+        key.Update(factory->NewFromStdString(ikey).GetTaggedValue());
+
+        // test has
+        bool has= JSAPITreeSet::Has(thread, tset, key);
+        EXPECT_EQ(has, false);
+    }
+
+    for (uint32_t i = REMOVE_SIZE; i < NODE_NUMBERS; i++) {
+        std::string ikey = myKey + std::to_string(i);
+        key.Update(factory->NewFromStdString(ikey).GetTaggedValue());
+
+        // test has
+        bool has= JSAPITreeSet::Has(thread, tset, key);
+        EXPECT_EQ(has, true);
+    }
+}
+
+HWTEST_F_L0(JSAPITreeSetTest, TreeSetClear)
+{
+    constexpr uint32_t NODE_NUMBERS = 8;
+    ObjectFactory *factory = thread->GetEcmaVM()->GetFactory();
+    JSMutableHandle<JSTaggedValue> key(thread, JSTaggedValue::Undefined());
+
+    // test TaggedTreeSet
+    JSHandle<JSAPITreeSet> tset(thread, CreateTreeSet());
+    std::string myKey("mykey");
+    for (uint32_t i = 0; i < NODE_NUMBERS; i++) {
+        std::string ikey = myKey + std::to_string(i);
+        key.Update(factory->NewFromStdString(ikey).GetTaggedValue());
+        JSAPITreeSet::Add(thread, tset, key);
+    }
+    EXPECT_EQ(tset->GetSize(), NODE_NUMBERS);
+
+    JSAPITreeSet::Clear(thread, tset);
+    EXPECT_EQ(tset->GetSize(), 0);
+    for (uint32_t i = 0; i < NODE_NUMBERS; i++) {
+        std::string ikey = myKey + std::to_string(i);
+        key.Update(factory->NewFromStdString(ikey).GetTaggedValue());
+
+        // test has
+        bool has= JSAPITreeSet::Has(thread, tset, key);
+        EXPECT_EQ(has, false);
+    }
+}
+
+HWTEST_F_L0(JSAPITreeSetTest, TreeSetPop)
+{
+    constexpr uint32_t NODE_NUMBERS = 8;
+    ObjectFactory *factory = thread->GetEcmaVM()->GetFactory();
+    JSMutableHandle<JSTaggedValue> key(thread, JSTaggedValue::Undefined());
+
+    // test TaggedTreeSet
+    JSHandle<JSAPITreeSet> tset(thread, CreateTreeSet());
+    std::string myKey("mykey");
+    for (uint32_t i = 0; i < NODE_NUMBERS; i++) {
+        std::string ikey = myKey + std::to_string(i);
+        key.Update(factory->NewFromStdString(ikey).GetTaggedValue());
+        JSAPITreeSet::Add(thread, tset, key);
+    }
+    EXPECT_EQ(tset->GetSize(), NODE_NUMBERS);
+
+    // test popFirst
+    std::string fkey = myKey + std::to_string(0);
+    key.Update(factory->NewFromStdString(fkey).GetTaggedValue());
+    JSTaggedValue fvalue = JSAPITreeSet::PopFirst(thread, tset);
+    EXPECT_EQ(tset->GetSize(), NODE_NUMBERS - 1);
+    EXPECT_EQ(fvalue, key.GetTaggedValue());
+    bool has= JSAPITreeSet::Has(thread, tset, key);
+    EXPECT_EQ(has, false);
+
+    // test popLast
+    std::string lkey = myKey + std::to_string(NODE_NUMBERS - 1);
+    key.Update(factory->NewFromStdString(lkey).GetTaggedValue());
+    JSTaggedValue lvalue = JSAPITreeSet::PopLast(thread, tset);
+    EXPECT_EQ(tset->GetSize(), NODE_NUMBERS - 2);
+    EXPECT_EQ(lvalue, key.GetTaggedValue());
+    has= JSAPITreeSet::Has(thread, tset, key);
+    EXPECT_EQ(has, false);
+}
+
+HWTEST_F_L0(JSAPITreeSetTest, JSAPITreeSetIterator)
+{
+    constexpr uint32_t NODE_NUMBERS = 8;
+    ObjectFactory *factory = thread->GetEcmaVM()->GetFactory();
+    JSHandle<JSAPITreeSet> tset(thread, CreateTreeSet());
+
+    JSMutableHandle<JSTaggedValue> key(thread, JSTaggedValue::Undefined());
+    for (int i = 0; i < NODE_NUMBERS; i++) {
+        key.Update(JSTaggedValue(i));
+        JSAPITreeSet::Add(thread, tset, key);
+    }
+
+    // test key or value
+    JSHandle<JSTaggedValue> keyIter(factory->NewJSAPITreeSetIterator(tset, IterationKind::KEY));
+    JSHandle<JSTaggedValue> valueIter(factory->NewJSAPITreeSetIterator(tset, IterationKind::VALUE));
+    JSMutableHandle<JSTaggedValue> keyIterResult(thread, JSTaggedValue::Undefined());
+    JSMutableHandle<JSTaggedValue> valueIterResult(thread, JSTaggedValue::Undefined());
+    for (int i = 0; i < NODE_NUMBERS / 2; i++) {
+        keyIterResult.Update(JSIterator::IteratorStep(thread, keyIter).GetTaggedValue());
+        valueIterResult.Update(JSIterator::IteratorStep(thread, valueIter).GetTaggedValue());
+        EXPECT_EQ(i, JSIterator::IteratorValue(thread, keyIterResult)->GetInt());
+        EXPECT_EQ(i, JSIterator::IteratorValue(thread, valueIterResult)->GetInt());
+    }
+
+    // test key and value
+    JSHandle<JSTaggedValue> indexKey(thread, JSTaggedValue(0));
+    JSHandle<JSTaggedValue> elementKey(thread, JSTaggedValue(1));
+    JSHandle<JSTaggedValue> iter(factory->NewJSAPITreeSetIterator(tset, IterationKind::KEY_AND_VALUE));
+    JSMutableHandle<JSTaggedValue> iterResult(thread, JSTaggedValue::Undefined());
+    JSMutableHandle<JSTaggedValue> result(thread, JSTaggedValue::Undefined());
+    for (int i = 0; i < NODE_NUMBERS; i++) {
+        iterResult.Update(JSIterator::IteratorStep(thread, iter).GetTaggedValue());
+        result.Update(JSIterator::IteratorValue(thread, iterResult).GetTaggedValue());
+        EXPECT_EQ(i, JSObject::GetProperty(thread, result, indexKey).GetValue()->GetInt());
+        EXPECT_EQ(i, JSObject::GetProperty(thread, result, elementKey).GetValue()->GetInt());
+    }
+
+    // test delete
+    key.Update(JSTaggedValue(NODE_NUMBERS / 2));
+    bool success = JSAPITreeSet::Delete(thread, tset, key);
+    EXPECT_EQ(success, true);
+    for (int i = NODE_NUMBERS / 2 + 1; i < NODE_NUMBERS; i++) {
+        keyIterResult.Update(JSIterator::IteratorStep(thread, keyIter).GetTaggedValue());
+        valueIterResult.Update(JSIterator::IteratorStep(thread, valueIter).GetTaggedValue());
+        EXPECT_EQ(i, JSIterator::IteratorValue(thread, keyIterResult)->GetInt());
+        EXPECT_EQ(i, JSIterator::IteratorValue(thread, valueIterResult)->GetInt());
+    }
+
+    // test set
+    key.Update(JSTaggedValue(NODE_NUMBERS));
+    JSAPITreeSet::Add(thread, tset, key);
+    keyIterResult.Update(JSIterator::IteratorStep(thread, keyIter).GetTaggedValue());
+    EXPECT_EQ(NODE_NUMBERS, JSIterator::IteratorValue(thread, keyIterResult)->GetInt());
+
+    // test end
+    keyIterResult.Update(JSIterator::IteratorStep(thread, keyIter).GetTaggedValue());
+    EXPECT_EQ(JSTaggedValue::False(), keyIterResult.GetTaggedValue());
+}
+}  // namespace panda::test
---
+++ test/moduletest/container/container_treeset.js
@@ -0,0 +1,88 @@
+/*
+ * Copyright (c) 2021 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+var fastset = undefined;
+if (globalThis["ArkPrivate"] != undefined) {
+    fastset = ArkPrivate.Load(ArkPrivate.TreeSet);
+}
+
+let set = new fastset();
+set.add("aa");
+set.add("bb");
+
+print("### test TreeSet start ###")
+// test has, out: true
+print("test has, out:", set.length == 2 && set.has("aa") && set.has("bb") && !set.has("cc"));
+
+set.add("cc");
+// test getFirstKey and getLastKey, out: true
+print("test getFirstKey and getLastKey, out:", set.getFirstValue() == "aa" && set.getLastValue() == "cc");
+// test getLowerValue and getHigherValue out: true
+print("test getLowerValue and getHigherValue out:", set.getLowerValue("bb") == "aa" &&
+      set.getLowerValue("aa") == undefined && set.getHigherValue("bb") == "cc" && set.getHigherValue("cc") == undefined);
+
+// test values, out: true
+let iteratorSetValues = set.values();
+print("test values, out:", iteratorSetValues.next().value == "aa" && iteratorSetValues.next().value == "bb" &&
+      iteratorSetValues.next().value == "cc" && iteratorSetValues.next().value == undefined);
+// test entries, out: [cc, cc], undefined
+let iteratorSetEntries = set.entries();
+iteratorSetEntries.next().value;
+iteratorSetEntries.next().value;
+print("test entries, out:", iteratorSetEntries.next().value);
+print(iteratorSetEntries.next().value);
+
+// test forof, out: aa, bb, cc
+print("test forof, out:");
+for (const item of set) {
+    print(item);
+}
+
+// test forin, out:
+print("test forin, out:");
+for (const item in set) {
+    print(item);
+}
+
+// test forEach, out:
+let setFlag = false;
+print("test forEach, out:");
+function TestForEach(value, key, set) {
+    setFlag= set.has(key) && set.has(value);
+    if (!setFlag) {
+        print(false);
+    }
+}
+set.forEach(TestForEach);
+
+// test isEmpty, out: false
+print("test isEmpty, out:", set.isEmpty());
+
+set.add("ee");
+set.add("dd");
+// test popFirst and popLast, out: true
+print("test popFirst and popLast, out:", set.length == 5 && set.popFirst() == "aa" &&
+      set.popLast() == "ee" && !set.has("aa"));
+// test remove, out: true
+print("test remove, out:", set.remove("bb") && set.length == 2 && !set.has("bb"));
+// test clear, out: true
+set.clear();
+print("test clear, out:", set.length == 0 && !set.has("cc") && set.isEmpty());
+
+try {
+    set["aa"] = 3;
+} catch (e) {
+    print(e);
+}
