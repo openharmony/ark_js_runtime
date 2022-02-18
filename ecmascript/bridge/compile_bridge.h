216898aa69cdbce4068f2abd5c6413fb19952af5
songzhengchao <songzhengchao@huawei.com>
2022-02-18 10:19:45 +0800

    init base arm64 ok
    Signed-off-by: songzhengchao <songzhengchao@huawei.com>
    Change-Id: I63577a40fdf970554eb7cc1c826600944823a50e

---
+++ ecmascript/bridge/compile_bridge.h
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2021 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#ifndef ECMASCRIPT_COMPILER_BRIDGE_H
+#define ECMASCRIPT_COMPILER_BRIDGE_H
+
+#include "ecmascript/js_method.h"
+#include "ecmascript/js_tagged_value.h"
+#include "ecmascript/js_handle.h"
+#include "ecmascript/js_thread.h"
+
+namespace panda::ecmascript {
+
+extern "C" uint64_t InvokeJSFunctionEntry(uintptr_t glue, uintptr_t prevFp, uint32_t expectedNumArgs,
+                               uint32_t actualNumArgs, const JSTaggedType argV[], uintptr_t codeAddr);
+
+}  // panda::ecmascript
+#endif
