2be4f7cd10817a3b85315b022c92759d9613562a
y00576111 <yaojian16@huawei.com>
2022-03-14 21:07:16 +0800

    add testcase for module
    add testcase for module interface,
    GetModuleValue/StoreModuleValue/AddStarExportEntry/AddIndirectExportEntry/AddLocalExportEntry/AddImportEntry
    
    issue: https://gitee.com/openharmony/ark_js_runtime/issues/I4WP00
    
    Signed-off-by: y00576111 <yaojian16@huawei.com>
    Change-Id: Ib97b577999f8111cbde25c020a515e85eeb624d1

--- ecmascript/tests/ecma_module_test.cpp
+++ ecmascript/tests/ecma_module_test.cpp
@@ -15,6 +15,7 @@
 
 #include "ecmascript/global_env.h"
 #include "ecmascript/js_locale.h"
+#include "ecmascript/module/js_module_source_text.h"
 #include "ecmascript/tagged_dictionary.h"
 #include "ecmascript/tests/test_helper.h"
 
@@ -46,4 +47,145 @@ public:
     ecmascript::EcmaHandleScope *scope {nullptr};
     JSThread *thread {nullptr};
 };
+
+/*
+ * Feature: Module
+ * Function: AddImportEntry
+ * SubFunction: AddImportEntry
+ * FunctionPoints: Add import entry
+ * CaseDescription: Add two import item and check module import entries size
+ */
+HWTEST_F_L0(EcmaModuleTest, AddImportEntry)
+{
+    ObjectFactory *objectFactory = thread->GetEcmaVM()->GetFactory();
+    JSHandle<SourceTextModule> module = objectFactory->NewSourceTextModule();
+    JSHandle<ImportEntry> importEntry1 = objectFactory->NewImportEntry();
+    SourceTextModule::AddImportEntry(thread, module, importEntry1);
+    JSHandle<ImportEntry> importEntry2 = objectFactory->NewImportEntry();
+    SourceTextModule::AddImportEntry(thread, module, importEntry2);
+    JSHandle<TaggedArray> importEntries(thread, module->GetImportEntries());
+    EXPECT_TRUE(importEntries->GetLength() == 2);
+}
+
+/*
+ * Feature: Module
+ * Function: AddLocalExportEntry
+ * SubFunction: AddLocalExportEntry
+ * FunctionPoints: Add local export entry
+ * CaseDescription: Add two local export item and check module local export entries size
+ */
+HWTEST_F_L0(EcmaModuleTest, AddLocalExportEntry)
+{
+    ObjectFactory *objectFactory = thread->GetEcmaVM()->GetFactory();
+    JSHandle<SourceTextModule> module = objectFactory->NewSourceTextModule();
+    JSHandle<ExportEntry> exportEntry1 = objectFactory->NewExportEntry();
+    SourceTextModule::AddLocalExportEntry(thread, module, exportEntry1);
+    JSHandle<ExportEntry> exportEntry2 = objectFactory->NewExportEntry();
+    SourceTextModule::AddLocalExportEntry(thread, module, exportEntry2);
+    JSHandle<TaggedArray> localExportEntries(thread, module->GetLocalExportEntries());
+    EXPECT_TRUE(localExportEntries->GetLength() == 2);
+}
+
+/*
+ * Feature: Module
+ * Function: AddIndirectExportEntry
+ * SubFunction: AddIndirectExportEntry
+ * FunctionPoints: Add indirect export entry
+ * CaseDescription: Add two indirect export item and check module indirect export entries size
+ */
+HWTEST_F_L0(EcmaModuleTest, AddIndirectExportEntry)
+{
+    ObjectFactory *objectFactory = thread->GetEcmaVM()->GetFactory();
+    JSHandle<SourceTextModule> module = objectFactory->NewSourceTextModule();
+    JSHandle<ExportEntry> exportEntry1 = objectFactory->NewExportEntry();
+    SourceTextModule::AddIndirectExportEntry(thread, module, exportEntry1);
+    JSHandle<ExportEntry> exportEntry2 = objectFactory->NewExportEntry();
+    SourceTextModule::AddIndirectExportEntry(thread, module, exportEntry2);
+    JSHandle<TaggedArray> indirectExportEntries(thread, module->GetIndirectExportEntries());
+    EXPECT_TRUE(indirectExportEntries->GetLength() == 2);
+}
+
+/*
+ * Feature: Module
+ * Function: StarExportEntries
+ * SubFunction: StarExportEntries
+ * FunctionPoints: Add start export entry
+ * CaseDescription: Add two start export item and check module start export entries size
+ */
+HWTEST_F_L0(EcmaModuleTest, AddStarExportEntry)
+{
+    ObjectFactory *objectFactory = thread->GetEcmaVM()->GetFactory();
+    JSHandle<SourceTextModule> module = objectFactory->NewSourceTextModule();
+    JSHandle<ExportEntry> exportEntry1 = objectFactory->NewExportEntry();
+    SourceTextModule::AddStarExportEntry(thread, module, exportEntry1);
+    JSHandle<ExportEntry> exportEntry2 = objectFactory->NewExportEntry();
+    SourceTextModule::AddStarExportEntry(thread, module, exportEntry2);
+    JSHandle<TaggedArray> startExportEntries(thread, module->GetStarExportEntries());
+    EXPECT_TRUE(startExportEntries->GetLength() == 2);
+}
+
+/*
+ * Feature: Module
+ * Function: StoreModuleValue
+ * SubFunction: StoreModuleValue/GetModuleValue
+ * FunctionPoints: store a module export item in module
+ * CaseDescription: Simulated implementation of "export foo as bar", set foo as "hello world",
+ *                  use "import bar" in same js file
+ */
+HWTEST_F_L0(EcmaModuleTest, StoreModuleValue)
+{
+    ObjectFactory* objFactory = thread->GetEcmaVM()->GetFactory();
+    CString localName = "foo";
+    CString exportName = "bar";
+    CString value = "hello world";
+
+    JSHandle<JSTaggedValue> localNameHandle = JSHandle<JSTaggedValue>::Cast(objFactory->NewFromString(localName));
+    JSHandle<JSTaggedValue> exportNameHandle = JSHandle<JSTaggedValue>::Cast(objFactory->NewFromString(exportName));
+    JSHandle<JSTaggedValue> defaultValue = thread->GlobalConstants()->GetHandledUndefined();
+    JSHandle<ExportEntry> exportEntry =
+        objFactory->NewExportEntry(exportNameHandle, defaultValue, defaultValue, localNameHandle);
+    JSHandle<SourceTextModule> module = objFactory->NewSourceTextModule();
+    SourceTextModule::AddLocalExportEntry(thread, module, exportEntry);
+
+    JSHandle<JSTaggedValue> storeKey = JSHandle<JSTaggedValue>::Cast(objFactory->NewFromString(localName));
+    JSHandle<JSTaggedValue> valueHandle = JSHandle<JSTaggedValue>::Cast(objFactory->NewFromString(value));
+    module->StoreModuleValue(thread, storeKey, valueHandle);
+
+    JSHandle<JSTaggedValue> loadKey = JSHandle<JSTaggedValue>::Cast(objFactory->NewFromString(exportName));
+    JSTaggedValue loadValue = module->GetModuleValue(thread, loadKey.GetTaggedValue(), false);
+    EXPECT_EQ(valueHandle.GetTaggedValue(), loadValue);
+}
+
+/*
+ * Feature: Module
+ * Function: GetModuleValue
+ * SubFunction: StoreModuleValue/GetModuleValue
+ * FunctionPoints: load module value from module
+ * CaseDescription: Simulated implementation of "export default let foo = 'hello world'",
+ *                  use "import C from 'xxx' to get default value"
+ */
+HWTEST_F_L0(EcmaModuleTest, GetModuleValue)
+{
+    ObjectFactory* objFactory = thread->GetEcmaVM()->GetFactory();
+    JSHandle<JSTaggedValue> defaultValue = thread->GlobalConstants()->GetHandledUndefined();
+    // export entry
+    CString exportLocalName = "*default*";
+    CString exportName = "default";
+    CString exportValue = "hello world";
+    JSHandle<JSTaggedValue> exportLocalNameHandle =
+        JSHandle<JSTaggedValue>::Cast(objFactory->NewFromString(exportLocalName));
+    JSHandle<JSTaggedValue> exportNameHandle =
+        JSHandle<JSTaggedValue>::Cast(objFactory->NewFromString(exportName));
+    JSHandle<ExportEntry> exportEntry =
+        objFactory->NewExportEntry(exportNameHandle, defaultValue, defaultValue, exportLocalNameHandle);
+    JSHandle<SourceTextModule> moduleExport = objFactory->NewSourceTextModule();
+    SourceTextModule::AddLocalExportEntry(thread, moduleExport, exportEntry);
+    // store module value
+    JSHandle<JSTaggedValue> exportValueHandle = JSHandle<JSTaggedValue>::Cast(objFactory->NewFromString(exportValue));
+    moduleExport->StoreModuleValue(thread, exportLocalNameHandle, exportValueHandle);
+
+    JSTaggedValue importDefaultValue =
+        moduleExport->GetModuleValue(thread, exportLocalNameHandle.GetTaggedValue(), false);
+    EXPECT_EQ(exportValueHandle.GetTaggedValue(), importDefaultValue);
+}
 }  // namespace panda::ecmascript
