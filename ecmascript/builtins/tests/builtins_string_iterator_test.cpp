b37dab64a3e1baa36fdefa41ed23e72559318786
hjzhangcm <zhanghaijun20@huawei.com>
2022-03-24 14:15:54 +0800

    add Test Case for BuiltinsStringIterator
    Signed-off-by: hjzhangcm <zhanghaijun20@huawei.com>

---
+++ ecmascript/builtins/tests/builtins_string_iterator_test.cpp
@@ -0,0 +1,141 @@
+/*
+ * Copyright (c) 2021 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "ecmascript/builtins/builtins_string_iterator.h"
+#include "ecmascript/js_iterator.h"
+#include "ecmascript/js_string_iterator.h"
+#include "ecmascript/tests/test_helper.h"
+
+using namespace panda::ecmascript;
+using namespace panda::ecmascript::builtins;
+
+namespace panda::test {
+class BuiltinsStringIteratorTest : public testing::Test {
+public:
+    static void SetUpTestCase()
+    {
+        GTEST_LOG_(INFO) << "SetUpTestCase";
+    }
+
+    static void TearDownTestCase()
+    {
+        GTEST_LOG_(INFO) << "TearDownCase";
+    }
+
+    void SetUp() override
+    {
+        TestHelper::CreateEcmaVMWithScope(instance, thread, scope);
+    }
+
+    void TearDown() override
+    {
+        TestHelper::DestroyEcmaVMWithScope(instance, scope);
+    }
+
+    PandaVM *instance {nullptr};
+    EcmaHandleScope *scope {nullptr};
+    JSThread *thread {nullptr};
+};
+
+static JSTaggedValue CreateBuiltinsJSStringIterator(JSThread *thread, const CString keyCStr)
+{
+    EcmaVM *ecmaVM = thread->GetEcmaVM();
+    ObjectFactory *factory = ecmaVM->GetFactory();
+
+    JSHandle<EcmaString> string = factory->NewFromString(&keyCStr[0]);
+    JSHandle<JSStringIterator> stringIterator = JSStringIterator::CreateStringIterator(thread, string);
+    EXPECT_TRUE(*stringIterator != nullptr);
+
+    return stringIterator.GetTaggedValue();
+}
+
+// Single char16_t Basic Multilingual plane character
+HWTEST_F_L0(BuiltinsStringIteratorTest, Next_001)
+{
+    auto globalConst = thread->GlobalConstants();
+    CString string = "没有";
+    JSHandle<JSStringIterator> stringIterator =
+        JSHandle<JSStringIterator>(thread, CreateBuiltinsJSStringIterator(thread, string));
+    JSHandle<JSTaggedValue> valueStr = globalConst->GetHandledValueString();
+    
+    auto ecmaRuntimeCallInfo1 = TestHelper::CreateEcmaRuntimeCallInfo(thread, JSTaggedValue::Undefined(), 4);
+    ecmaRuntimeCallInfo1->SetFunction(JSTaggedValue::Undefined());
+    ecmaRuntimeCallInfo1->SetThis(stringIterator.GetTaggedValue());
+
+    [[maybe_unused]] auto prev = TestHelper::SetupFrame(thread, ecmaRuntimeCallInfo1.get());
+    BuiltinsStringIterator::Next(ecmaRuntimeCallInfo1.get());
+    TestHelper::TearDownFrame(thread, prev);
+    EXPECT_EQ(stringIterator->GetStringIteratorNextIndex(), 1U);
+
+    auto ecmaRuntimeCallInfo2 = TestHelper::CreateEcmaRuntimeCallInfo(thread, JSTaggedValue::Undefined(), 4);
+    ecmaRuntimeCallInfo2->SetFunction(JSTaggedValue::Undefined());
+    ecmaRuntimeCallInfo2->SetThis(stringIterator.GetTaggedValue());
+
+    prev = TestHelper::SetupFrame(thread, ecmaRuntimeCallInfo2.get());
+    BuiltinsStringIterator::Next(ecmaRuntimeCallInfo2.get());
+    TestHelper::TearDownFrame(thread, prev);
+    EXPECT_EQ(stringIterator->GetStringIteratorNextIndex(), 2U);
+
+    auto ecmaRuntimeCallInfo3 = TestHelper::CreateEcmaRuntimeCallInfo(thread, JSTaggedValue::Undefined(), 4);
+    ecmaRuntimeCallInfo3->SetFunction(JSTaggedValue::Undefined());
+    ecmaRuntimeCallInfo3->SetThis(stringIterator.GetTaggedValue());
+
+    prev = TestHelper::SetupFrame(thread, ecmaRuntimeCallInfo3.get());
+    JSTaggedValue result = BuiltinsStringIterator::Next(ecmaRuntimeCallInfo3.get());
+    TestHelper::TearDownFrame(thread, prev);
+
+    JSHandle<JSTaggedValue> resultObj(thread, result);
+    EXPECT_TRUE(JSObject::GetProperty(thread, JSHandle<JSObject>(thread, result), valueStr).GetValue()->IsUndefined());
+}
+
+// character with lead surrogate and trail surrogate character
+HWTEST_F_L0(BuiltinsStringIteratorTest, Next_002)
+{
+    auto globalConst = thread->GlobalConstants();
+    CString string = "没𠕇";
+    JSHandle<JSStringIterator> stringIterator =
+        JSHandle<JSStringIterator>(thread, CreateBuiltinsJSStringIterator(thread, string));
+    JSHandle<JSTaggedValue> valueStr = globalConst->GetHandledValueString();
+    
+    auto ecmaRuntimeCallInfo1 = TestHelper::CreateEcmaRuntimeCallInfo(thread, JSTaggedValue::Undefined(), 4);
+    ecmaRuntimeCallInfo1->SetFunction(JSTaggedValue::Undefined());
+    ecmaRuntimeCallInfo1->SetThis(stringIterator.GetTaggedValue());
+
+    [[maybe_unused]] auto prev = TestHelper::SetupFrame(thread, ecmaRuntimeCallInfo1.get());
+    BuiltinsStringIterator::Next(ecmaRuntimeCallInfo1.get());
+    TestHelper::TearDownFrame(thread, prev);
+    EXPECT_EQ(stringIterator->GetStringIteratorNextIndex(), 1U);
+
+    auto ecmaRuntimeCallInfo2 = TestHelper::CreateEcmaRuntimeCallInfo(thread, JSTaggedValue::Undefined(), 4);
+    ecmaRuntimeCallInfo2->SetFunction(JSTaggedValue::Undefined());
+    ecmaRuntimeCallInfo2->SetThis(stringIterator.GetTaggedValue());
+
+    prev = TestHelper::SetupFrame(thread, ecmaRuntimeCallInfo2.get());
+    BuiltinsStringIterator::Next(ecmaRuntimeCallInfo2.get());
+    TestHelper::TearDownFrame(thread, prev);
+    EXPECT_EQ(stringIterator->GetStringIteratorNextIndex(), 3U);
+
+    auto ecmaRuntimeCallInfo3 = TestHelper::CreateEcmaRuntimeCallInfo(thread, JSTaggedValue::Undefined(), 4);
+    ecmaRuntimeCallInfo3->SetFunction(JSTaggedValue::Undefined());
+    ecmaRuntimeCallInfo3->SetThis(stringIterator.GetTaggedValue());
+
+    prev = TestHelper::SetupFrame(thread, ecmaRuntimeCallInfo3.get());
+    JSTaggedValue result = BuiltinsStringIterator::Next(ecmaRuntimeCallInfo3.get());
+    TestHelper::TearDownFrame(thread, prev);
+
+    JSHandle<JSTaggedValue> resultObj(thread, result);
+    EXPECT_TRUE(JSObject::GetProperty(thread, JSHandle<JSObject>(thread, result), valueStr).GetValue()->IsUndefined());
+}
+}  // namespace panda::test
