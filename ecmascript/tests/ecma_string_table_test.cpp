7161a6974f13865c04aa54b56ceb11d765446afb
zhaozhibo <zhaozhibo3@huawei.com>
2022-03-25 17:54:47 +0800

    add test case for utf_helper,accessor_data,ecmastring_table
    Signed-off-by: zhaozhibo <zhaozhibo3@huawei.com>

--- ecmascript/tests/ecma_string_table_test.cpp
+++ ecmascript/tests/ecma_string_table_test.cpp
@@ -126,41 +126,4 @@ HWTEST_F_L0(EcmaStringTableTest, GetOrInternString_EcmaString)
     EXPECT_STREQ(ecmaStrGetPtr->GetCString().get(), "hello world");
     EXPECT_TRUE(ecmaStrGetPtr->IsInternString());
 }
-
-/**
- * @tc.name: SweepWeakReference
- * @tc.desc: Scan all string constants in the intern pool, erase the constants that are not referenced by the
-             object, and convert the referenced object type that is not EcmaString to EcmaString type.
- * @tc.type: FUNC
- * @tc.require:
- */
-HWTEST_F_L0(EcmaStringTableTest, SweepWeakReference)
-{
-    EcmaVM *vm = thread->GetEcmaVM();
-    EcmaStringTable *table = vm->GetEcmaStringTable();
-    ObjectFactory *factory = vm->GetFactory();
-    auto heap = thread->GetEcmaVM()->GetHeap();
-
-    size_t oldSizebase = heap->GetOldSpace()->GetHeapObjectSize();
-    uint8_t utf8Data1[] = {0x68, 0x65, 0x6c, 0x6c, 0x6f}; // " hello "
-    uint8_t utf8Data2[] = {0x77, 0x6f, 0x72, 0x6c, 0x64}; // " world "
-    EcmaString *ecmaStrHandle1 = table->GetOrInternString(utf8Data1, sizeof(utf8Data1), true);
-    EcmaString *ecmaStrHandle2 = table->GetOrInternString(utf8Data2, sizeof(utf8Data2), true);
-    EXPECT_TRUE(ecmaStrHandle1->IsInternString() && ecmaStrHandle2->IsInternString());
-
-    for (int i = 0; i < 1024; i++) {
-        factory->NewFromUtf8(utf8Data1, sizeof(utf8Data1));
-    }
-    size_t oldSizeBefore = 0;
-    oldSizeBefore = heap->GetOldSpace()->GetHeapObjectSize();
-    EXPECT_EQ(oldSizeBefore, oldSizebase);
-
-    WeakRootVisitor visitor = []() {
-        return reinterpret_cast<TaggedObject *>(ToUintPtr(nullptr));
-    };
-    ecmaStrHandle2 = nullptr;
-    table->SweepWeakReference(visitor);
-    size_t oldSizeAfter = heap->GetOldSpace()->GetHeapObjectSize();
-    EXPECT_EQ(oldSizeBefore, oldSizeAfter);
-}
 }  // namespace panda::test
