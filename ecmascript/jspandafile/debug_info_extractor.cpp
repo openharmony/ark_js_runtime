b1cff3c61aed12e7cf42e6c88dd1c0cc33035ded
wengchangcheng <wengchangcheng@huawei.com>
2022-04-07 11:34:43 +0800

    fix compiler error of line_program_state.h
    delete line_program_state.h, and use line_number_program.h
    
    issue: https://gitee.com/openharmony/ark_js_runtime/issues/I51H6M
    
    Signed-off-by: wengchangcheng <wengchangcheng@huawei.com>
    Change-Id: Iae277768bbe3a4c3945d6416be84a53a0456c5d5

--- ecmascript/jspandafile/debug_info_extractor.cpp
+++ ecmascript/jspandafile/debug_info_extractor.cpp
@@ -19,13 +19,14 @@
 #include "libpandabase/utils/utf.h"
 #include "libpandafile/class_data_accessor-inl.h"
 #include "libpandafile/debug_data_accessor-inl.h"
-#include "libpandafile/line_program_state.h"
+#include "libpandafile/line_number_program.h"
 
 namespace panda::ecmascript {
 using panda::panda_file::ClassDataAccessor;
 using panda::panda_file::DebugInfoDataAccessor;
 using panda::panda_file::LineNumberProgramItem;
 using panda::panda_file::LineProgramState;
+using panda::panda_file::LineNumberProgramProcessor;
 using panda::panda_file::MethodDataAccessor;
 using panda::panda_file::ProtoDataAccessor;
 
@@ -39,68 +40,95 @@ DebugInfoExtractor::DebugInfoExtractor(const JSPandaFile *jsPandaFile)
     Extract(jsPandaFile->GetPandaFile());
 }
 
-class LineNumberProgramProcessor {
+class LineNumberProgramHandler {
 public:
-    LineNumberProgramProcessor(LineProgramState state, const uint8_t *program) : state_(state), program_(program) {}
+    explicit LineNumberProgramHandler(LineProgramState *state) : state_(state) {}
+    ~LineNumberProgramHandler() = default;
 
-    ~LineNumberProgramProcessor() = default;
+    NO_COPY_SEMANTIC(LineNumberProgramHandler);
+    NO_MOVE_SEMANTIC(LineNumberProgramHandler);
 
-    NO_COPY_SEMANTIC(LineNumberProgramProcessor);
-    NO_MOVE_SEMANTIC(LineNumberProgramProcessor);
-
-    void Process()
+    LineProgramState *GetState() const
     {
-        auto opcode = ReadOpcode();
-        lnt_.push_back({static_cast<int32_t>(state_.GetAddress()), static_cast<int32_t>(state_.GetLine())});
-        while (opcode != Opcode::END_SEQUENCE) {
-            switch (opcode) {
-                case Opcode::ADVANCE_LINE: {
-                    HandleAdvanceLine();
-                    break;
-                }
-                case Opcode::ADVANCE_PC: {
-                    HandleAdvancePc();
-                    break;
-                }
-                case Opcode::SET_FILE: {
-                    HandleSetFile();
-                    break;
-                }
-                case Opcode::SET_SOURCE_CODE: {
-                    HandleSetSourceCode();
-                    break;
-                }
-                case Opcode::SET_PROLOGUE_END:
-                case Opcode::SET_EPILOGUE_BEGIN:
-                    break;
-                case Opcode::START_LOCAL: {
-                    HandleStartLocal();
-                    break;
-                }
-                case Opcode::START_LOCAL_EXTENDED: {
-                    HandleStartLocalExtended();
-                    break;
-                }
-                case Opcode::RESTART_LOCAL: {
-                    LOG(FATAL, ECMASCRIPT) << "Opcode RESTART_LOCAL is not supported";
-                    break;
-                }
-                case Opcode::END_LOCAL: {
-                    HandleEndLocal();
-                    break;
-                }
-                case Opcode::SET_COLUMN: {
-                    HandleSetColumn();
-                    break;
-                }
-                default: {
-                    HandleSpecialOpcode(opcode);
-                    break;
-                }
-            }
-            opcode = ReadOpcode();
-        }
-        // process end offset
+        return state_;
+    }
+
+    void ProcessBegin()
+    {
+        lnt_.push_back({state_->GetAddress(), state_->GetLine()});
+    }
+
+    void ProcessEnd()
+    {
+    }
+
+    bool HandleAdvanceLine(int32_t lineDiff) const
+    {
+        state_->AdvanceLine(lineDiff);
+        return true;
+    }
+
+    bool HandleAdvancePc(uint32_t pcDiff) const
+    {
+        state_->AdvancePc(pcDiff);
+        return true;
+    }
+
+    bool HandleSetFile(uint32_t sourceFileId) const
+    {
+        state_->SetFile(sourceFileId);
+        return true;
+    }
+
+    bool HandleSetSourceCode(uint32_t sourceCodeId) const
+    {
+        state_->SetSourceCode(sourceCodeId);
+        return true;
+    }
+
+    bool HandleSetPrologueEnd() const
+    {
+        return true;
+    }
+
+    bool HandleSetEpilogueBegin() const
+    {
+        return true;
+    }
+
+    bool HandleStartLocal(int32_t regNumber, uint32_t nameId, [[maybe_unused]] uint32_t typeId)
+    {
+        const char *name = GetStringFromConstantPool(state_->GetPandaFile(), nameId);
+        lvt_.insert(std::make_pair(name, regNumber));
+        return true;
+    }
+
+    bool HandleStartLocalExtended(int32_t regNumber, uint32_t nameId, [[maybe_unused]] uint32_t typeId,
+                                  [[maybe_unused]] uint32_t typeSignatureId)
+    {
+        const char *name = GetStringFromConstantPool(state_->GetPandaFile(), nameId);
+        lvt_.insert(std::make_pair(name, regNumber));
+        return true;
+    }
+
+    bool HandleEndLocal([[maybe_unused]] int32_t regNumber)
+    {
+        return true;
+    }
+
+    bool HandleSetColumn(int32_t columnNumber)
+    {
+        state_->SetColumn(columnNumber);
+        cnt_.push_back({state_->GetAddress(), state_->GetColumn()});
+        return true;
+    }
+
+    bool HandleSpecialOpcode(uint32_t pcOffset, int32_t lineOffset)
+    {
+        state_->AdvancePc(pcOffset);
+        state_->AdvanceLine(lineOffset);
+        lnt_.push_back({state_->GetAddress(), state_->GetLine()});
+        return true;
     }
 
     LineNumberTable GetLineNumberTable() const
@@ -108,117 +136,30 @@ public:
         return lnt_;
     }
 
-    ColumnNumberTable GetColumnNumberTable() const
-    {
-        return cnt_;
-    }
-
     LocalVariableTable GetLocalVariableTable() const
     {
         return lvt_;
     }
 
+    ColumnNumberTable GetColumnNumberTable() const
+    {
+        return cnt_;
+    }
+
     const uint8_t *GetFile() const
     {
-        return state_.GetFile();
+        return state_->GetFile();
     }
 
     const uint8_t *GetSourceCode() const
     {
-        return state_.GetSourceCode();
+        return state_->GetSourceCode();
     }
 
 private:
     using Opcode = LineNumberProgramItem::Opcode;
 
-    Opcode ReadOpcode()
-    {
-        auto opcode = static_cast<Opcode>(*program_);
-        ++program_;  // NOLINT(cppcoreguidelines-pro-bounds-pointer-arithmetic)
-        return opcode;
-    }
-
-    int32_t ReadRegisterNumber()
-    {
-        auto [regiserNumber, n, isFull] = leb128::DecodeSigned<int32_t>(program_);
-        LOG_IF(!isFull, FATAL, ECMASCRIPT) << "Cannot read a register number";
-        program_ += n;  // NOLINT(cppcoreguidelines-pro-bounds-pointer-arithmetic)
-        return regiserNumber;
-    }
-
-    void HandleAdvanceLine()
-    {
-        auto line_diff = state_.ReadSLeb128();
-        state_.AdvanceLine(line_diff);
-    }
-
-    void HandleAdvancePc()
-    {
-        auto pc_diff = state_.ReadULeb128();
-        state_.AdvancePc(pc_diff);
-    }
-
-    void HandleSetFile()
-    {
-        state_.SetFile(state_.ReadULeb128());
-    }
-
-    void HandleSetSourceCode()
-    {
-        state_.SetSourceCode(state_.ReadULeb128());
-    }
-
-    void HandleSetPrologueEnd() {}
-
-    void HandleSetEpilogueBegin() {}
-
-    void HandleStartLocal()
-    {
-        auto regNumber = ReadRegisterNumber();
-        auto nameIndex = state_.ReadULeb128();
-        [[maybe_unused]] auto typeIndex = state_.ReadULeb128();
-        const char *name = GetStringFromConstantPool(state_.GetPandaFile(), nameIndex);
-        lvt_.insert(std::make_pair(name, regNumber));
-    }
-
-    void HandleStartLocalExtended()
-    {
-        auto regNumber = ReadRegisterNumber();
-        auto nameIndex = state_.ReadULeb128();
-        [[maybe_unused]] auto typeIndex = state_.ReadULeb128();
-        [[maybe_unused]] auto typeSignatureIndex = state_.ReadULeb128();
-        const char *name = GetStringFromConstantPool(state_.GetPandaFile(), nameIndex);
-        lvt_.insert(std::make_pair(name, regNumber));
-    }
-
-    void HandleEndLocal()
-    {
-        [[maybe_unused]] auto regNumber = ReadRegisterNumber();
-        // process end offset
-    }
-
-    void HandleSetColumn()
-    {
-        auto cn = state_.ReadULeb128();
-        state_.SetColumn(cn);
-        cnt_.push_back({static_cast<int32_t>(state_.GetAddress()), static_cast<int32_t>(state_.GetColumn())});
-    }
-
-    void HandleSpecialOpcode(LineNumberProgramItem::Opcode opcode)
-    {
-        ASSERT(static_cast<uint8_t>(opcode) >= LineNumberProgramItem::OPCODE_BASE);
-
-        auto adjustOpcode = static_cast<uint8_t>(static_cast<uint8_t>(opcode) - LineNumberProgramItem::OPCODE_BASE);
-        auto pcOffset = static_cast<uint32_t>(adjustOpcode / LineNumberProgramItem::LINE_RANGE);
-        int32_t lineOffset =
-            static_cast<int32_t>(adjustOpcode) % LineNumberProgramItem::LINE_RANGE + LineNumberProgramItem::LINE_BASE;
-        state_.AdvancePc(pcOffset);
-        state_.AdvanceLine(lineOffset);
-        lnt_.push_back({static_cast<int32_t>(state_.GetAddress()), static_cast<int32_t>(state_.GetLine())});
-    }
-
-    LineProgramState state_;
-    const uint8_t *program_;
+    LineProgramState *state_;
     LineNumberTable lnt_;
     LocalVariableTable lvt_;
     ColumnNumberTable cnt_;
@@ -250,16 +191,17 @@ void DebugInfoExtractor::Extract(const panda_file::File *pf)
             LineProgramState state(pandaFile, sourceFileId.value_or(panda_file::File::EntityId(0)), dda.GetLineStart(),
                                    dda.GetConstantPool());
 
-            LineNumberProgramProcessor programProcessor(state, program);
+            LineNumberProgramHandler handler(&state);
+            LineNumberProgramProcessor<LineNumberProgramHandler> programProcessor(program, &handler);
             programProcessor.Process();
 
             panda_file::File::EntityId methodId = mda.GetMethodId();
-            const char *sourceFile = utf::Mutf8AsCString(programProcessor.GetFile());
-            const char *sourceCode = utf::Mutf8AsCString(programProcessor.GetSourceCode());
+            const char *sourceFile = utf::Mutf8AsCString(handler.GetFile());
+            const char *sourceCode = utf::Mutf8AsCString(handler.GetSourceCode());
             methods_.insert(std::make_pair(methodId.GetOffset(), MethodDebugInfo {sourceFile, sourceCode,
-                                           programProcessor.GetLineNumberTable(),
-                                           programProcessor.GetColumnNumberTable(),
-                                           programProcessor.GetLocalVariableTable()}));
+                                           handler.GetLineNumberTable(),
+                                           handler.GetColumnNumberTable(),
+                                           handler.GetLocalVariableTable()}));
         });
     }
 }
