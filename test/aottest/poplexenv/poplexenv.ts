d8fc2948e432b12800a22e3501491166590d1c84
Hailiang Hu <allenpas@huawei.com>
2022-04-28 16:53:19 +0800

    AOT Lowering Test
    1. Test BC of createiterresultobj, getiterator, instanceof,
    isin, newlexenv, poplexenv, suspendgenerator, typeof,
    throwifsupernotcorrectcall, throwundefindeifhole.
    2. Update test framework to support com stub, debug mode and log mode.
    
    Issue: #I55HDG
    
    Change-Id: I68670978916da5a12e2d7b8abb59f636ac7c8250
    Signed-off-by: Hailiang Hu <allenpas@huawei.com>

---
+++ test/aottest/poplexenv/poplexenv.ts
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+declare function print(str:any):string;
+function fn(x:any) {
+  let a = [];
+  for (let p in x) {
+    a.push(function () { return p; });
+	print(p);
+  }
+  return a;
+}
+
+fn("hello world");
+fn([1, 2, 3, 4]);
