0e92153add0235af4019e44f766fc6a5b30f90d1
lijincheng <lijincheng13@huawei.com>
2022-05-12 14:40:26 +0800

    Add Aot Test Case
    1.Add Aot test cases for 17 bytecodes:
    CREATEARRAYWITHBUFFER_PREF_IMM16, CREATEOBJECTWITHBUFFER_PREF_IMM16
    CREATEREGEXPWITHLITERAL_PREF_ID32_IMM8, DEFINEASYNCFUNC_PREF_ID16_IMM16_V8
    DEFINEGENERATORFUNC_PREF_ID16_IMM16_V8, GETNEXTPROPNAME_PREF_V8
    GETTEMPLATEOBJECT_PREF_V8, LDLEXVARDYN_PREF_IMM4_IMM4, LDLEXVARDYN_PREF_IMM8_IMM8
    LDLEXVARDYN_PREF_IMM16_IMM16, STLEXVARDYN_PREF_IMM4_IMM4_V8, STLEXVARDYN_PREF_IMM8_IMM8_V8
    STLEXVARDYN_PREF_IMM16_IMM16_V8, SETOBJECTWITHPROTO_PREF_V8_V8, STOWNBYINDEX_PREF_V8_IMM32
    STOWNBYNAME_PREF_ID32_V8, STOWNBYVALUE_PREF_V8_V8
    
    issue:https://gitee.com/openharmony/ark_js_runtime/issues/I573OV
    
    Signed-off-by: lijincheng <lijincheng13@huawei.com>

---
+++ test/aottest/createregexpwithliteral/createregexpwithliteral.ts
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+declare function print(str:boolean):boolean;
+var re = /abc+/;
+var str1 = "abcc";
+var str2 = "ac";
+var res1 = re.test(str1);
+var res2 = re.test(str2);
+print(res1);
+print(res2);
