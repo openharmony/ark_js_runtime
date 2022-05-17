6b22539e9d8398e8d4dbe7d3dd772d9d73034786
getingke <getingke@huawei.com>
2022-05-17 10:23:58 +0800

    fixed for aarch64 assembler invoke jsfunction entry
    Signed-off-by: getingke <getingke@huawei.com>
    Change-Id: I844e55a895441fd188c2c2c357907afd0998779c

--- ecmascript/trampoline/trampoline.h
+++ ecmascript/trampoline/trampoline.h
@@ -25,5 +25,8 @@ extern "C" JSTaggedType JSFunctionEntry(uintptr_t glue, uintptr_t prevFp, uint32
     uint32_t actualNumArgs, const JSTaggedType argV[], uintptr_t codeAddr);
 extern "C" JSTaggedType OptimizedCallOptimized(uintptr_t glue, uint32_t expectedNumArgs,
     uint32_t actualNumArgs, uintptr_t codeAddr, ...);
+
+using JSFunctionEntryType = uint64_t (*)(uintptr_t glue, uintptr_t prevFp, uint32_t expectedNumArgs,
+                                        uint32_t actualNumArgs, const JSTaggedType argV[], uintptr_t codeAddr);
 }  // panda::ecmascript
 #endif  // ECMASCRIPT_BRIDGE_H
