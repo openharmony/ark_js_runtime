c0c4198086e426c9d75ddcb13467c5d7a0bf6c81
getingke <getingke@huawei.com>
2022-06-21 14:31:59 +0800

    fixed for debug call with argv
    Signed-off-by: getingke <getingke@huawei.com>
    Change-Id: I2e08fac9b385212af8d19c413a2d00aa1671e4bd

--- ecmascript/stubs/runtime_stubs.h
+++ ecmascript/stubs/runtime_stubs.h
@@ -49,8 +49,8 @@ using JSFunctionEntryType = uint64_t (*)(uintptr_t glue, uintptr_t prevFp, uint3
     V(PushCallNewAndDispatchNative)          \
     V(PushCallIRangeAndDispatchNative)       \
     V(PushCallIThisRangeAndDispatch)         \
-    V(CallOptimizedJSFunction)                           \
-    V(JSCallWithArgV)                       \
+    V(CallOptimizedJSFunction)               \
+    V(JSCallWithArgV)                        \
     V(ResumeRspAndDispatch)                  \
     V(ResumeRspAndReturn)                    \
     V(ResumeCaughtFrameAndDispatch)          \
@@ -59,7 +59,7 @@ using JSFunctionEntryType = uint64_t (*)(uintptr_t glue, uintptr_t prevFp, uint3
     V(CallGetter)                            \
     V(CallRuntimeWithArgv)                   \
     V(JSCall)                                \
-    V(JSProxyCallInternalWithArgV)                        \
+    V(JSProxyCallInternalWithArgV)           \
     V(JSFunctionEntry)                       \
     V(CallBuiltinTrampoline)                 \
     V(OptimizedCallOptimized)
